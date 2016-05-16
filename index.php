30e8451c71ae199574b662637d552512d0b50438
sirotama <magicsirotama@gmail.com>
2016-05-16 18:53:47 +0100

    update_nameに対応した

--- index.php
+++ index.php
@@ -30,12 +30,17 @@ $body = ['since-cursor'=>$latestCursor];
 $getMention = Requests::post('http://himasaku.misskey.tk/posts/mentions/show',$header,$body);
 
 $mentions = json_decode($getMention->body);
-
+if($mentions != []){
+	$latestCursor = $mentions[0]->cursor;
+	file_put_contents('cursor.txt',$latestCursor);
+};
+$mentions = array_reverse($mentions);
 $mentionText = "";
 foreach($mentions as $mention){
 	$mentionText = $mention->text;
 	$mentionID = $mention->id;
 	$replybody = '@'.$mention->user->screenName.' ';
+	//mecab
 	if(preg_match('/^@srtm mecab (.+)$/',$mentionText,$match)){
 		$igopost = $igo->parse($match[1]);
 		foreach($igopost as $var){
@@ -46,6 +51,17 @@ foreach($mentions as $mention){
 		$header = ['Cookie'=>'hmsk='.$hmskToken,'csrf-token'=>$csrf];
 		$postbody = ['text'=>$replybody,'in-reply-to-post-id' => $mentionID];
 		$createPost = Requests::post('http://himasaku.misskey.tk/posts/reply',$header,$postbody);
+
+		sleep(3);
+	}
+	//update_name
+	if(preg_match('/^@srtm update_name (.+)$/',$mentionText,$match)){
+		$header = ['Cookie'=>'hmsk='.$hmskToken,'csrf-token'=>$csrf];
+		$renamebody = ['name'=>$match[1]];
+		$postbody = ['text'=>$replybody.$match[1].'に改名させられました。','in-reply-to-post-id' => $mentionID];
+		$updateName = Requests::post('http://himasaku.misskey.tk/account/name/update',$header,$renamebody);
+		$createPost = Requests::post('http://himasaku.misskey.tk/posts/reply',$header,$postbody);
+
 		sleep(3);
 	}
 }
